7b64cae3e6fb346a31e2a156b9a28a4d317b3da8
lstypka <lstypka@future-processing.com>
2016-06-08 20:58:12 +0200

    #20 created testNg example project

---
+++ jevidence-example-testng/src/test/java/pl/lstypka/jevidence/example/testng/SubtractMathServiceTest.java
@@ -0,0 +1,49 @@
+package pl.lstypka.jevidence.example.testng;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * Created by Lukasz on 2016-06-08.
+ */
+public class SubtractMathServiceTest extends AbstractTest {
+
+    private MathService mathService = new MathService();
+
+    @Test
+    @Parameters({"firstNumber", "secondNumber", "sum"})
+    public void paramTest(Integer firstNumber, Integer secondNumber, Integer sum) {
+        // given
+
+        // when
+        Integer result = mathService.substract(firstNumber, secondNumber);
+
+        // then
+        assertThat(result).isEqualTo(sum);
+    }
+
+    @Test
+    public void shouldSubtractTwoNumbers() {
+        // given
+
+        // when
+        Integer result = mathService.substract(5, 3);
+
+        // then
+        assertThat(result).isEqualTo(2);
+    }
+
+
+    @Test
+    public void shouldSubtractThreeNumbers() {
+        // given
+
+        // when
+        Integer result = mathService.substract(11, mathService.substract(5, 1));
+
+        // then
+        assertThat(result).isEqualTo(7);
+    }
+}
